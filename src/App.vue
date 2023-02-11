b2249b73d1a8d63850d1483440da71aba14afbaa
anonymousvegan <majjunjulavgust@gmail.com>
2023-02-11 20:52:51 +0100

    finish styles

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,38 @@ import Message from './components/Message.vue';
 import profilePhoto from './assets/profile.jpg';
 import phone from './assets/phone.svg';
 import video from './assets/video.svg';
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+const messages = ref({});
+const userId = ref("1");
+
+onMounted(async () => {
+
+	try {
+		let res = await axios.get('http://localhost:3000/messages');
+		messages.value = res.data;
+	}
+
+	catch (err) {
+		console.log(err);
+	}
+});
 
 </script>
 
 <template>
 	<div class="cont">
 		<header>
+
 			<div class="left">
 				<img :src="profilePhoto" alt="">
 			</div>
+
 			<div class="center">
 				<h3>Nikola Matković</h3>
 			</div>
+
 			<div class="right">
 				<img :src="phone" alt="">
 				<img :src="video" alt="">
@@ -25,14 +45,10 @@ import video from './assets/video.svg';
 					<div class="dot"></div>
 				</div>
 			</div>
+
 		</header>
 		<main>
-			<Message side="left" message="" />
-			<Message side="right" message="Hello World" />
-			<Message side="left" message="Hello World" />
-			<Message side="right" message="Hello World" />
-			<Message side="left" message="Hello World" />
-
+			<Message :userId="userId" v-for="message in messages" :key="message.id" :message="message" />
 		</main>
 	</div>
 </template>
@@ -40,12 +56,15 @@ import video from './assets/video.svg';
 <style lang="scss">
 
 :root{
-	--profile-image-size : 65px;
+	--profile-image-size: 50px;
+	--header-padding-size: 10px;
+	--icons-size : 20px;
+	--header-size : var(--profile-image-size) + var(--header-padding-size) * 2; 
 }
 
 #app{
-	width: 100%;
-	height: 100%;
+	width: 100vw;
+	height: 100vh;
 	display: grid;
 	place-items: center;
 }
@@ -54,7 +73,7 @@ header{
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
-	padding: 20px;
+	padding: var(--header-padding-size);
 	background: hsl(261, 100%, 80%);
 	color: white;
 	font-size: 15px;
@@ -78,8 +97,8 @@ header{
 		align-items: center;
 	
 		img {
-			width: 30px;
-			height: 30px;
+			width: var(--icons-size);
+			height: var(--icons-size);
 			margin-right: 10px;	
 		}
 
@@ -106,6 +125,36 @@ header{
 	border: 1px solid black;
 	border-radius: 10px;
 	overflow: hidden;
-
+	display: grid;
+	grid-template-rows: var(--header-size) 1fr;
+	resize: both;
 }
+
+main {
+	display: flex;
+	flex-direction: column;
+	height: auto;
+	flex-shrink: 0;
+	overflow-y: auto;
+	gap: 10px;
+	padding: 10px;
+	background-color: rgb(22, 22, 22);
+}
+
+::-webkit-scrollbar {
+	width: 5px;
+	background-color: rgb(131, 131, 184);
+}
+
+::-webkit-scrollbar-button {
+	width: 0px;
+	height: 0px;
+}
+
+::-webkit-scrollbar-thumb {
+	background-color: rgb(208, 0, 0);
+	width: 5px;
+	height: 100px;
+}
+
 </style>
--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,17 +1,37 @@
 <template>
-    <h1>{{ message }}</h1>
+    <div class="message" :class="userId === message.user.id ? 'right' : 'left'">
+        <p> {{ message.text }}</p>
+    </div>
 </template>
-
 
 <script setup>
 
 defineProps({
-    message: String
+    message: String,
+    userId: [Number, String],
 })
 
 </script>
 
 
 <style scoped>
+
+.message{
+    margin-left: 20px;
+    padding: 10px;
+    border-radius: 10px;
+    white-space: pre-wrap;
+    color: rgb(17, 27, 33);
+    line-height: 20px;
+	width: 80%;
+}
+.left {
+    background: hsl(261, 100%, 90%);
+}
+.right {
+    background: hsl(190, 100%, 90%);
+    align-self: flex-end;
+}
+
 
 </style>

7105e0d54a2754dbfc682f11968d912de67d6fed
anonymousvegan <majjunjulavgust@gmail.com>
2023-04-07 21:30:14 +0200

    .

--- src/functions/getMessages.js
+++ src/functions/getMessages.js
@@ -5,7 +5,7 @@ export default async function getMessages() {
     try {
         let res = await axios.get(`${config.url}getMessages.php`, {
             headers :{
-                "ngrok-skip-browser-warning": "69420",
+                "Access-Control-Allow-Origin": "*",
             }
         });
         return res.data;

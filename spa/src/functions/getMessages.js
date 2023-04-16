f8b85f4ee2005a27e58b0560740e58d429ca9eaa
anonymousvegan <majjunjulavgust@gmail.com>
2023-04-16 21:36:33 +0200

    Password check now works

--- spa/src/functions/getMessages.js
+++ spa/src/functions/getMessages.js
@@ -4,9 +4,10 @@ import config from '../config';
 export default async function getMessages(password) {
     try {
         console.log(password)
-        let res = await axios.post(`${config.url}getMessages.php`, {
-            password : password,
-        });
+        let data = new FormData();
+        data.append("password", password);
+        let res = await axios.post(`${config.url}getMessages.php`, data);
+        console.log(res)
         return res.data;
     }
     catch (err) {
